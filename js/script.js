1501ded343eadc12a55112e8e26d50e29116bc91
Copter <copterpanyphon@gmail.com>
2024-11-22 06:57:36 +0700

    asdf

--- js/script.js
+++ js/script.js
@@ -30,3 +30,36 @@ Kong.addEventListener('mouseleave', changeToOriginalImage);
 
 Kong.addEventListener('touchstart', changeToSecondImage);
 Kong.addEventListener('touchend', changeToOriginalImage);
+
+
+
+window.addEventListener('load', function () {
+  const loadingScreen = document.getElementById('loading-screen');
+  const mainContent = document.getElementById('main-content');
+
+  setTimeout(() => {
+    loadingScreen.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 3000);
+});
+
+
+let clickCount = 0;
+let timer = null;
+const RESET_DELAY = 200;
+
+const countDisplay = document.getElementById('count');
+
+const resetCount = () => {
+  clickCount = 0;
+  countDisplay.textContent = `Clicks: ${clickCount}`;
+};
+
+Kong.addEventListener('click', () => {
+  clickCount++;
+  countDisplay.textContent = `Clicks: ${clickCount}`;
+
+  // Clear the existing timer and start a new one
+  if (timer) clearTimeout(timer);
+  timer = setTimeout(resetCount, RESET_DELAY);
+});
